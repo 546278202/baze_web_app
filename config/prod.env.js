310ba3dc5c0c249d8a2e7d5d0d8cd5a8249936b5
wanlong <546278202@qq.com>
2020-12-31 13:37:31 +0800

    清楚数据

--- config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  API_HOST: '"https://zhangwoyun.feijizhe.com"'//注意点与上面类似
-}
--- config/test.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const devEnv = require('./dev.env')
-
-module.exports = {
-  NODE_ENV: '"production"',
-  API_HOST: '"https://zhangwoyun.feijizhe.com"'//注意点与上面类似
-}
